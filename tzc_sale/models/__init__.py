963faf6a7678d7f2c28e0e8056118fa93964c7d5
Jigar A. (Odoo) <jam@odoo.com>
2019-03-20 08:23:50 -0700

    [RM] removed tzc_sale app

--- tzc_sale/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import sale_catalog
-from . import product_template
-from . import sale_order
-from . import mail_compose_message
-from . import website
--- tzc_sale/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import product_template_wizard
-from . import sale_catalog_wizard
